a1d70525cb3bef375d298e5977175ad1f87039aa
Ahmed Al-Asaad <ahmed.alasaad@gmail.com>
2018-03-28 22:40:24 -0400

    Add UserModel

---
+++ AspnetOverPosting/AspnetOverPosting/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetOverPosting.Models
+{
+    public class UserModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Role { get; set; }
+    }
+}
