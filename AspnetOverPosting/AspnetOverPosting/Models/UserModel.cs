c799cdb45b578f02f3ac04a27f13e92c7be9f582
Ahmed Al-Asaad <ahmed.alasaad@gmail.com>
2018-03-28 23:42:40 -0400

    Add Id to user model

--- AspnetOverPosting/AspnetOverPosting/Models/UserModel.cs
+++ AspnetOverPosting/AspnetOverPosting/Models/UserModel.cs
@@ -7,6 +7,7 @@ namespace AspnetOverPosting.Models
 {
     public class UserModel
     {
+        public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Role { get; set; }
